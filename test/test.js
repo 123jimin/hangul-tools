f41bc2e385a7d6ed9418118aa3bed184c8ed8b0b
Jimin Park <pjm950@gmail.com>
2015-05-20 18:51:04 +0900

    Added type tests

--- test/test.js
+++ test/test.js
@@ -4,10 +4,14 @@ var assert = require('assert'),
 describe('HanTools', function(){
 	// More like sanity checks
 	describe('#constants', function(){
+		assert.strictEqual(typeof HanTools.CHOSEONG, 'string', "Choseong list should be string.");
+		assert.strictEqual(typeof HanTools.JUNGSEONG, 'string', "Jungseong list should be string.");
+		assert.strictEqual(typeof HanTools.JONGSEONG, 'string', "Jongseong list should be string.");
+		assert.strictEqual(typeof HanTools.JONGSEONG_EMPTY, 'string', "JONGSEONG_EMPTY should be string.");
 		assert.strictEqual(HanTools.CHOSEONG, "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ", "Choseong list should be exact.");
 		assert.strictEqual(HanTools.JUNGSEONG , "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ", "Jungseong list should be exact.");
-		assert.strictEqual(typeof HanTools.JONGSEONG, 'string', "Jongseong list should be string.");
 		assert.strictEqual(HanTools.JONGSEONG.slice(1), "ㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ", "Jongseong list should be exact.");
 		assert.strictEqual(HanTools.JONGSEONG_EMPTY, HanTools.JONGSEONG[0], "The first character of the jongseong list should be JONGSEONG_EMPTY.");
+		assert.strictEqual(HanTools.JONGSEONG_EMPTY.length, 1, "JONGSEONG_EMPTY's length must be 1");
 	});
 });
