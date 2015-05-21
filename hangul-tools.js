6e61749b4523853834cf03cdfb5ec19af51bb68c
Jimin Park <pjm950@gmail.com>
2015-05-21 16:43:00 +0900

    Edited the code a little

--- hangul-tools.js
+++ hangul-tools.js
@@ -7,13 +7,20 @@ var HANGUL_FIRST = '가',
 	HANGUL_FIRST_CODE = HANGUL_FIRST.charCodeAt(0),
 	HANGUL_LAST_CODE = HANGUL_LAST.charCodeAt(0);
 
+var CHOSEONG = "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ",
+	JUNGSEONG = "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ",
+	JONGSEONG = "Xㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ",
+	CHOSEONG_LEN = CHOSEONG.length,
+	JUNGSEONG_LEN = JUNGSEONG.length,
+	JONGSEONG_LEN = JONGSEONG.length;
+
 var root = this;
 var _old_hantools = this.HanTools;
 
 var HanTools = {
-	'CHOSEONG': "ㄱㄲㄴㄷㄸㄹㅁㅂㅃㅅㅆㅇㅈㅉㅊㅋㅌㅍㅎ",
-	'JUNGSEONG': "ㅏㅐㅑㅒㅓㅔㅕㅖㅗㅘㅙㅚㅛㅜㅝㅞㅟㅠㅡㅢㅣ",
-	'JONGSEONG': "Xㄱㄲㄳㄴㄵㄶㄷㄹㄺㄻㄼㄽㄾㄿㅀㅁㅂㅄㅅㅆㅇㅈㅊㅋㅌㅍㅎ",
+	'CHOSEONG': CHOSEONG,
+	'JUNGSEONG': JUNGSEONG,
+	'JONGSEONG': JONGSEONG,
 	'JONGSEONG_EMPTY': "X",
 	'isHangul': function(s){
 		for(var i=0; i<s.length; i++)
@@ -21,11 +28,11 @@ var HanTools = {
 		return true;
 	},
 	'toChoseong': function(s){
-		var r, i, c, CHOSEONG = HanTools.CHOSEONG;
+		var r, i, c;
 		if(s.length == 1){
 			c = s.charCodeAt(0);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				return CHOSEONG[0|(c-HANGUL_FIRST_CODE)/588];
+				return CHOSEONG[0|(c-HANGUL_FIRST_CODE)/(JUNGSEONG_LEN*JONGSEONG_LEN)];
 			else
 				return s;
 		}
@@ -33,18 +40,18 @@ var HanTools = {
 		for(i=0; i<s.length; i++){
 			c = s.charCodeAt(i);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				r += CHOSEONG[0|(c-HANGUL_FIRST_CODE)/588];
+				r += CHOSEONG[0|(c-HANGUL_FIRST_CODE)/(JUNGSEONG_LEN*JONGSEONG_LEN)];
 			else
 				r += s[i];
 		}
 		return r;
 	},
 	'toJungseong': function(s){
-		var r, i, c, JUNGSEONG = HanTools.JUNGSEONG;
+		var r, i, c;
 		if(s.length == 1){
 			c = s.charCodeAt(0);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				return JUNGSEONG[0|(c-HANGUL_FIRST_CODE)/28%21];
+				return JUNGSEONG[0|(c-HANGUL_FIRST_CODE)/JONGSEONG_LEN%JUNGSEONG_LEN];
 			else
 				return s;
 		}
@@ -52,18 +59,18 @@ var HanTools = {
 		for(i=0; i<s.length; i++){
 			c = s.charCodeAt(i);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				r += JUNGSEONG[0|(c-HANGUL_FIRST_CODE)/28%21];
+				r += JUNGSEONG[0|(c-HANGUL_FIRST_CODE)/JONGSEONG_LEN%JUNGSEONG_LEN];
 			else
 				r += s[i];
 		}
 		return r;
 	},
 	'toJongseong': function(s){
-		var r, i, c, JONGSEONG = HanTools.JONGSEONG;
+		var r, i, c;
 		if(s.length == 1){
 			c = s.charCodeAt(0);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				return JONGSEONG[0|(c-HANGUL_FIRST_CODE)%28];
+				return JONGSEONG[0|(c-HANGUL_FIRST_CODE)%JONGSEONG_LEN];
 			else
 				return s;
 		}
@@ -71,7 +78,7 @@ var HanTools = {
 		for(i=0; i<s.length; i++){
 			c = s.charCodeAt(i);
 			if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE)
-				r += JONGSEONG[(c-HANGUL_FIRST_CODE)%28];
+				r += JONGSEONG[(c-HANGUL_FIRST_CODE)%JONGSEONG_LEN];
 			else
 				r += s[i];
 		}
@@ -81,16 +88,16 @@ var HanTools = {
 		var c = s.charCodeAt(0);
 		if(c >= HANGUL_FIRST_CODE && c <= HANGUL_LAST_CODE){
 			c -= HANGUL_FIRST_CODE;
-			if(c % 28)
+			if(c % JONGSEONG_LEN)
 				return [
-					HanTools.CHOSEONG[0|c/588],
-					HanTools.JUNGSEONG[0|c/28%21],
-					HanTools.JONGSEONG[c%28]
+					CHOSEONG[0|c/(JUNGSEONG_LEN*JONGSEONG_LEN)],
+					JUNGSEONG[0|c/JONGSEONG_LEN%JUNGSEONG_LEN],
+					JONGSEONG[c%JONGSEONG_LEN]
 				];
 			else
 				return [
-					HanTools.CHOSEONG[0|c/588],
-					HanTools.JUNGSEONG[0|c/28%21]
+					CHOSEONG[0|c/(JUNGSEONG_LEN*JONGSEONG_LEN)],
+					JUNGSEONG[0|c/JONGSEONG_LEN%JUNGSEONG_LEN]
 				];
 		}else return s;
 	},
@@ -98,28 +105,20 @@ var HanTools = {
 		if(!s) return '';
 		var c = s.charCodeAt(0);
 		if(c < HANGUL_FIRST_CODE || c > HANGUL_LAST_CODE) return s;
-		switch(0|(c-HANGUL_FIRST_CODE)/28){
+		switch(0|(c-HANGUL_FIRST_CODE)/JONGSEONG_LEN){
 			// 녀, 뇨, 뉴, 니
-			case 48:
-			case 54:
-			case 59:
-			case 62:
+			case 48: case 54:
+			case 59: case 62:
 				c += 5292; break;
 			// 랴, 려, 례, 료, 류, 리
-			case 107:
-			case 111:
-			case 112:
-			case 117:
-			case 122:
-			case 125:
+			case 107: case 111:
+			case 112: case 117:
+			case 122: case 125:
 				c += 3528; break;
 			// 라, 래, 로, 뢰, 루, 르
-			case 105:
-			case 106:
-			case 113:
-			case 116:
-			case 118:
-			case 123:
+			case 105: case 106:
+			case 113: case 116:
+			case 118: case 123:
 				c -= 1764; break;
 		}
 		return String.fromCharCode(c) + s.slice(1);
