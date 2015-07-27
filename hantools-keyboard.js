0062d872f116b825533f158878ecb18fac1df8f2
Jimin Park <pjm950@gmail.com>
2015-07-27 18:52:48 +0900

    Added getKeySequences

--- hantools-keyboard.js
+++ hantools-keyboard.js
@@ -10,16 +10,103 @@ var _extend = function _extend(parent, f){
 	return child;
 };
 
+var _push_multi = function _push_multi(a, p){
+	p.forEach(function(e){a.push(e)});
+};
+
+var QWERTY_SHIFT = {
+	'`': '~', '1': '!', '2': '@', '3': '#', '4': '$', '5': '%', '6': '^', '7': '&', '8': '*', '9': '(', '0': ')',
+	'-': '_', '=': '+', '\\': '|', '[': '{', ']': '}', ';': ':', '\'': '\"', ',': '<', '.': '>', '/': '?'
+}, QWERTY_SHIFT_INV = {};
+
+var _qwerty_key = function(c){
+	if('A' <= c && c <= 'Z') return [c.toLowerCase(), true];
+	else if(c in QWERTY_SHIFT_INV) return [QWERTY_SHIFT_INV[c], true];
+	else return [c, false];
+};
+
+var _key_map_convert = function(s){
+	return s ? s.split('').map(function(c){return _qwerty_key(c);}) : [];
+};
+
+var DUBEOLSIK_MAP = [
+	"rRseEfaqQtTdwWczxvg".split('').map(function(c){
+		return c <= 'Z' ? [[c.toLowerCase(), true]] : [[c, false]];
+	}),
+	"k o i O j p u P h hk ho hl y n nj np nl b m ml l".split(' ').map(_key_map_convert),
+	" r R rt s sw sg e f fr fa fq ft fx fv fg a q qt t T d w c z x v g".split(' ').map(_key_map_convert)
+];
+
+var SEBEOLSIK_390_MAP = [
+	"k kk h u uu y i ; ;; n nn j l ll o 0 \' p m".split(' ').map(_key_map_convert),
+	"f r 6 R t c e 7 v /f /r /d 4 b 9t 9c 9d 5 g 8 d".split(' ').map(_key_map_convert),
+	" x F xq s s! S A w D C w3 wq wW wQ V z 3 X q 2 a ! Z E W Q 1".split(' ').map(_key_map_convert)
+];
+
+(function init_dict(){
+	for(var key in QWERTY_SHIFT) QWERTY_SHIFT_INV[QWERTY_SHIFT[key]] = key;
+})();
+
 var root = this;
 var _old_hantools_keyboard = this.HanTools_Keyboard;
 
 var HanTools_Keyboard = function INIT_HANTOOLS_KEYBOARD(HanTools){
 
+var _kor_key = function(map, c){
+	var ind = HanTools.CHOSEONG.indexOf(c);
+	if(ind >= 0) return map[0][ind];
+	ind = HanTools.JUNGSEONG.indexOf(c);
+	if(ind >= 0) return map[1][ind];
+	ind = HanTools.JONGSEONG.indexOf(c);
+	if(ind >= 0) return map[2][ind];
+	return map[3] && c in map[3] ? map[3][c] : [_qwerty_key(c)];
+};
+
 var Keyboard = function Keyboard(){};
 
 Keyboard.QWERTY = _extend(Keyboard);
+Keyboard.QWERTY.getKeySequence = function QWERTY_getKeySequence(str){
+	var i, seq = [];
+	for(i=0; i<str.length; i++) seq.push(_qwerty_key(str[i]));
+	return seq;
+};
+
 Keyboard.DUBEOLSIK = _extend(Keyboard);
+Keyboard.DUBEOLSIK.getKeySequence = function DUBEOLSIK_getKeySequence(str){
+	var seq = [];
+	str.split('').map(HanTools.disintegrate).forEach(function(part){
+		var ind;
+		if(typeof part == 'string') _push_multi(seq, _kor_key(DUBEOLSIK_MAP, part));
+		else part.forEach(function(c){_push_multi(seq, _kor_key(DUBEOLSIK_MAP, c));});
+	});
+	return seq;
+};
+
 Keyboard.SEBEOLSIK_390 = _extend(Keyboard);
+Keyboard.SEBEOLSIK_390.getKeySequence = function SEBEOLSIK_390_getKeySequence(str){
+	var seq = [];
+	str.split('').map(HanTools.disintegrate).forEach(function(part){
+		var ind;
+		if(typeof part == 'string'){
+			switch(part){
+				case 'ㅘ': seq.push(['v', false], ['f', false]); break;
+				case 'ㅙ': seq.push(['v', false], ['r', false]); break;
+				case 'ㅚ': seq.push(['v', false], ['d', false]); break;
+				case 'ㅝ': seq.push(['b', false], ['f', false]); break;
+				case 'ㅞ': seq.push(['b', false], ['c', false]); break;
+				case 'ㅟ': seq.push(['b', false], ['d', false]); break;
+				default: _push_multi(seq, _kor_key(SEBEOLSIK_390_MAP, part));
+			}
+		}else{
+			_push_multi(seq, SEBEOLSIK_390_MAP[0][HanTools.CHOSEONG.indexOf(part[0])]);
+			_push_multi(seq, SEBEOLSIK_390_MAP[1][HanTools.JUNGSEONG.indexOf(part[1])]);
+			if(part.length == 3) _push_multi(seq, SEBEOLSIK_390_MAP[2][HanTools.JONGSEONG.indexOf(part[2])]);
+		}
+	});
+	return seq;
+};
+
+Keyboard.getKeySequence = Keyboard.QWERTY.getKeySequence;
 
 return Keyboard;
 
--- test/keyboard.js
+++ test/keyboard.js
@@ -1,6 +1,15 @@
 var assert = require('assert'),
 	HanTools = require('../');
 
+var Keyboard = HanTools.Keyboard;
+
+var _simple_seq = function _simple_seq(s){
+	var i, a = [];
+	for(i=0; i<s.length; i+=2)
+		a.push([s[i], s[i+1]=='^']);
+	return a;
+};
+
 describe('HanTools', function(){
 	describe('#Keyboard', function(){
 		it("should have some important keyboards", function(){
@@ -8,6 +17,54 @@ describe('HanTools', function(){
 			assert.strictEqual(typeof HanTools.Keyboard.QWERTY, 'function');
 			assert.strictEqual(typeof HanTools.Keyboard.DUBEOLSIK, 'function');
 			assert.strictEqual(typeof HanTools.Keyboard.SEBEOLSIK_390, 'function');
+		});
+
+		describe('#convert()', function(){
+			it("should behave correctly", function(){
+
+			});
+		});
+		
+		describe('#QWERTY', function(){
+			describe('#getKeySequence()', function(){
+				it("should be able to get key sequences", function(){
+					assert.deepEqual(
+						Keyboard.QWERTY.getKeySequence("Hello, world!"),
+						_simple_seq("h^e_l_l_o_,_ _w_o_r_l_d_1^")
+					);
+				});
+			});
+		});
+
+		describe('#DUBEOLSIK', function(){
+			describe('#getKeySequence()', function(){
+				it("should be able to get key sequences for Hangul strings", function(){
+					assert.deepEqual(
+						Keyboard.DUBEOLSIK.getKeySequence("나랏말싸미 듕귁에 달아"),
+						_simple_seq("s_k_f_k_t_a_k_f_t^k_a_l_ _e_b_d_r_n_l_r_d_p_ _e_k_f_d_k_")
+					);
+					assert.deepEqual(
+						Keyboard.DUBEOLSIK.getKeySequence("ㅇㅅㅇ ㄲㄲ"),
+						_simple_seq("d_t_d_ _r^r^")
+					)
+				});
+
+			});
+		});
+
+		describe('#SEBEOLSIK_390', function(){
+			describe('#getKeySequence()', function(){
+				it("should be able to get key sequences for Hangul strings", function(){
+					assert.deepEqual(
+						Keyboard.SEBEOLSIK_390.getKeySequence("나랏말싸미 듕귁에 달아"),
+						_simple_seq("h_f_y_f_q_i_f_w_n_n_f_i_d_ _u_5_a_k_9_d_x_j_c_ _u_f_w_j_f_")
+					);
+					assert.deepEqual(
+						Keyboard.SEBEOLSIK_390.getKeySequence("ㅇㅅㅇ ㄲㄲ"),
+						_simple_seq("j_n_j_ _k_k_k_k_")
+					);
+				});
+			});
 		});
 	});
 });
